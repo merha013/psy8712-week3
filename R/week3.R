904dce1d7d886dc3221ba2d325bcfb7648e68c6a
Toni Merhar <merha013@umn.edu>
2024-02-03 10:08:16 -0600

    Week 3 project complete

--- R/week3.R
+++ R/week3.R
@@ -3,8 +3,8 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 # Data Import and Cleaning
 raw_df <- read.csv("../data/week3.csv", header=TRUE)
-raw_df$timeStart <- as.POSIXct(raw_df$timeStart) #I don't think this actually did anything as the time was already in this format.
-raw_df$timeEnd <- as.POSIXct(raw_df$timeEnd) #I don't think this actually did anything as the time was already in this format.
+raw_df$timeStart <- as.POSIXct(raw_df$timeStart) 
+raw_df$timeEnd <- as.POSIXct(raw_df$timeEnd) 
 clean_df <- subset(raw_df, raw_df$timeStart>"2017-06-30 23:59:59")
 clean_df <- subset(clean_df, clean_df$q6==1)
 
